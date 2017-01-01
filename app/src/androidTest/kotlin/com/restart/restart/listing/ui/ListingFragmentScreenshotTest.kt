8dc29cb44d7ec27c8a04e6db070df43884503d43
Sergio Gutierrez <sergio@gokarumi.com>
2017-01-01 22:20:25 +0100

    Create listing screenshot test. Clean up some unused files

---
+++ app/src/androidTest/kotlin/com/restart/restart/listing/ui/ListingFragmentScreenshotTest.kt
@@ -0,0 +1,21 @@
+package com.restart.restart.listing.ui
+
+import android.support.test.espresso.intent.rule.IntentsTestRule
+import com.facebook.testing.screenshot.Screenshot
+import com.restart.restart.main.ui.MainActivity
+import org.junit.Rule
+import org.junit.Test
+
+
+class ListingFragmentScreenshotTest {
+
+    @get:Rule var activityRule: IntentsTestRule<MainActivity> =
+        IntentsTestRule(MainActivity::class.java, true, false)
+
+    @Test
+    fun shouldRenderAllItemsWhenThereAreSeveralItemsAvailable() {
+        val activity = activityRule.launchActivity(null)
+
+        Screenshot.snapActivity(activity).record()
+    }
+}
