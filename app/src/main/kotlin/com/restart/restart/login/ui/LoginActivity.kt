d1ca591de25c7ed854887749538254bbdaee0ab9
Sergio Gutierrez <sergio@gokarumi.com>
2017-04-16 16:49:09 +0200

    Add back button icon

--- app/src/main/kotlin/com/restart/restart/login/ui/LoginActivity.kt
+++ app/src/main/kotlin/com/restart/restart/login/ui/LoginActivity.kt
@@ -3,11 +3,16 @@ package com.restart.restart.login.ui
 import android.os.Bundle
 import com.restart.restart.R
 import com.restart.restart.shared.ui.RestartActivity
+import kotlinx.android.synthetic.main.login.*
 
 class LoginActivity : RestartActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.action_bar_back_icon)
+        toolbar.navigationIcon = resources.getDrawable(R.drawable.action_bar_back_icon)
     }
 }
