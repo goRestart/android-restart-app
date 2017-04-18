a7f4614e98d5a27e3b30972c1b36284c50d9f2bf
Sergio Gutierrez <sergio@gokarumi.com>
2017-04-19 00:25:27 +0200

    Replace default font with OpenSans

--- app/src/main/kotlin/com/restart/restart/shared/ui/RestartActivity.kt
+++ app/src/main/kotlin/com/restart/restart/shared/ui/RestartActivity.kt
@@ -1,10 +1,12 @@
 package com.restart.restart.shared.ui
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.restart.restart.R
 import com.restart.restart.shared.RestartApplication
 import com.restart.restart.shared.di.DependencyContainer
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
 
 open class RestartActivity : AppCompatActivity() {
 
@@ -15,5 +17,9 @@ open class RestartActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val application = application as RestartApplication
         this.dependencyContainer = application.container
+    }
+
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase))
     }
 }
