adbcbf6fbff73f491e4664fe237541da6af518f4
Sergio Gutierrez <sergio@gokarumi.com>
2017-04-23 23:53:38 +0200

    Configure injections

--- app/src/main/kotlin/com/restart/restart/login/di/LoginContainer.kt
+++ app/src/main/kotlin/com/restart/restart/login/di/LoginContainer.kt
@@ -1,24 +1,43 @@
 package com.restart.restart.login.di
 
+import com.restart.restart.login.data.LoginDataSource
+import com.restart.restart.login.data.SessionTokenStorage
+import com.restart.restart.login.domain.model.Session
+import com.restart.restart.login.domain.usecase.Login
 import com.restart.restart.login.ui.LoginPresenter
 import com.restart.restart.login.ui.UnloggedUserPresenter
 import com.restart.restart.shared.di.DependencyContainer
 import java.lang.ref.WeakReference
 
-class LoginContainer(
+open class LoginContainer(
     private val dependencyContainer: DependencyContainer
 ) {
-
-    fun getPresenter(view: LoginPresenter.View): LoginPresenter {
+    open fun getPresenter(view: LoginPresenter.View): LoginPresenter {
         return LoginPresenter(
-            WeakReference(view)
+            WeakReference(view),
+            login,
+            dependencyContainer.useCaseExecutor
         )
     }
 
-    fun getUnloggedUserPresenter(view: UnloggedUserPresenter.View): UnloggedUserPresenter {
+    open fun getUnloggedUserPresenter(view: UnloggedUserPresenter.View): UnloggedUserPresenter {
         return UnloggedUserPresenter(
             WeakReference(view)
         )
     }
 
+    open val login: Login
+        get() = Login(session)
+
+    open val session: Session
+        get() = Session(
+            loginDataSource,
+            sessionTokenStorage
+        )
+
+    open val loginDataSource: LoginDataSource
+        get() = LoginDataSource()
+
+    open val sessionTokenStorage: SessionTokenStorage
+        get() = SessionTokenStorage(dependencyContainer.sharedPreferences)
 }
--- app/src/main/kotlin/com/restart/restart/shared/di/DependencyContainer.kt
+++ app/src/main/kotlin/com/restart/restart/shared/di/DependencyContainer.kt
@@ -1,15 +1,29 @@
 package com.restart.restart.shared.di
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Handler
 import com.restart.restart.listing.di.ListingContainer
 import com.restart.restart.login.di.LoginContainer
 import com.restart.restart.main.ui.MainPresenter
 import com.restart.restart.product.di.ProductContainer
 import com.restart.restart.product.ui.viewmodel.PlatformViewModelMapper
 import com.restart.restart.product.ui.viewmodel.PriceViewModelMapper
+import com.restart.restart.shared.domain.UseCaseExecutor
 import com.restart.restart.storage.ProductStorage
 import java.lang.ref.WeakReference
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
 
-class DependencyContainer {
+class DependencyContainer(
+    private val appContext: Context
+) {
+    var activityContext: Context? = null
+
+    val context: Context
+        get() = activityContext!!
 
     val listing: ListingContainer
         get() = ListingContainer(this)
@@ -33,4 +47,37 @@ class DependencyContainer {
 
     val platformViewModelMapper: PlatformViewModelMapper
         get() = PlatformViewModelMapper()
+
+    val useCaseExecutor: UseCaseExecutor
+        get() = UseCaseExecutor(
+            useCaseExecutorService,
+            useCaseHandler
+        )
+
+    val useCaseExecutorService: ExecutorService
+        get() {
+            val corePoolSize = Runtime.getRuntime().availableProcessors()
+            val maximumPoolSize = 8
+            val keepAliveTime: Long = 30
+            val unit = TimeUnit.SECONDS
+            val workQueue = LinkedBlockingQueue<Runnable>()
+
+            return ThreadPoolExecutor(
+                corePoolSize,
+                maximumPoolSize,
+                keepAliveTime,
+                unit,
+                workQueue
+            )
+        }
+
+    val useCaseHandler: Handler by lazy {
+        Handler()
+    }
+
+    val sharedPreferences: SharedPreferences
+        get() = context.getSharedPreferences(
+            "Restart App Shared Preferences",
+            Context.MODE_PRIVATE
+        )
 }
